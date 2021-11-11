bbccc24c0025fedf8367a662cc4f1e3a08be6e79
AMIR SOHEL <43946342+asb19@users.noreply.github.com>
2021-11-11 14:26:33 +0530

    feat(location): locations added

---
+++ prisma/migrations/20211110170113_location_schema/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "Country" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "phoneCode" TEXT NOT NULL,
+
+    CONSTRAINT "Country_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "State" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "countryId" INTEGER NOT NULL,
+
+    CONSTRAINT "State_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "City" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "stateId" INTEGER NOT NULL,
+
+    CONSTRAINT "City_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "State" ADD CONSTRAINT "State_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "City" ADD CONSTRAINT "City_stateId_fkey" FOREIGN KEY ("stateId") REFERENCES "State"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,53 @@
 import { PrismaClient } from '@prisma/client';
 import { genSalt, hash } from 'bcrypt';
+
+// import { jsonData } from '../location';
 const prisma = new PrismaClient();
 
 async function main() {
   const salt = await genSalt(10);
   const alice = await prisma.user.upsert({
     where: {
-      email: 'amir@gmail.com',
+      email: 'test12@gmail.com',
     },
     update: {},
     create: {
-      email: 'amir2gmail.com',
+      email: 'test123@gmail.com',
       Password: await hash('1234', salt),
     },
   });
 
   console.log({ alice });
+  // console.log(jsonData.name);
+
+  // const obj = jsonData;
+  // const countryName = obj.name;
+  // const phoneCode = obj.phone_code;
+  // const cnt = await prisma.country.create({
+  //   data: {
+  //     name: countryName,
+  //     phoneCode,
+  //   },
+  // });
+  // for (const state of obj.states) {
+  //   const stateEntry = await prisma.state.create({
+  //     data: {
+  //       name: state.name,
+  //       countryId: cnt.id,
+  //       fullName: state.name + ', ' + countryName,
+  //     },
+  //   });
+  //   for (const city of state.cities) {
+  //     await prisma.city.create({
+  //       data: {
+  //         stateId: stateEntry.id,
+  //         name: city.name,
+  //         fullName: city.name + ', ' + stateEntry.name + ', ' + countryName,
+  //       },
+  //     });
+  //     console.log(countryName + ':' + '--' + state.name + '--' + city.name);
+  //   }
+  // }
 }
 
 main()
