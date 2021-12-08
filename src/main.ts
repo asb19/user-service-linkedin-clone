5ad79d5003ee055a35a924fb1437999377555c30
AMIR SOHEL <43946342+asb19@users.noreply.github.com>
2021-12-08 18:02:16 +0530

    fix(main): rabbitmq connection commented

--- src/main.ts
+++ src/main.ts
@@ -7,24 +7,26 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const user = 'admin';
-  const password = 'admin';
-  const host = 'localhost:5672';
-  const queueName = 'AUTH_MESSAGE';
+  // const user = 'admin';
+  // const password = 'admin';
+  // const host = 'localhost:5672';
+  // const queueName = 'AUTH_MESSAGE';
 
-  await app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [`amqp://${user}:${password}@${host}`],
-      queue: queueName,
-      noAck: false,
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
+  // await app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.RMQ,
+  //   options: {
+  //     urls: [`amqp://${user}:${password}@${host}`],
+  //     queue: queueName,
+  //     noAck: false,
+  //     queueOptions: {
+  //       durable: true,
+  //     },
+  //   },
+  // });
 
-  await app.startAllMicroservices();
+  // await app.startAllMicroservices();
+
+  //TODO: create queue service
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
