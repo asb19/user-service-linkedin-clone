47ca736e15ad033231e17d67b6a603b96496ebc5
AMIR SOHEL <43946342+asb19@users.noreply.github.com>
2021-12-08 18:18:11 +0530

    fix(host): host change

--- src/guards/AuthGuard.ts
+++ src/guards/AuthGuard.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
     try {
       console.log('inside guard');
       const res = await this.httpService
-        .get(`http://localhost:3002/auth/check`, {
+        .get(`http://3.110.207.83:3002/auth/check`, {
           headers: {
             'x-auth-token': req.headers['authorization']?.split(' ')[1],
           },
