f9b48805cf1f16567b54bc3c798039c0dd78bdfc
AMIR SOHEL <amirsohel.as10@gmail.com>
2022-01-06 18:42:05 +0530

    fix(org): dto fix

--- src/organization/dto/createOrganisationDto.dto.ts
+++ src/organization/dto/createOrganisationDto.dto.ts
@@ -1,29 +1,31 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
-    OrganisationCreateDetailsDto,
-    OrgAwardsDetailsDto,
-    OrgContactDetailsDto,
-    OrgCourseDetailsDto,
-    OrgEventDetailsDto,
-    OrgTrainingDetailsDto,
+  OrganisationCreateDetailsDto,
+  OrgAwardsDetailsDto,
+  OrgContactDetailsDto,
+  OrgCourseDetailsDto,
+  OrgEventDetailsDto,
+  OrgTrainingDetailsDto,
 } from './organisationOtherDetails.sto';
 export class CreateOrganisationDto {
-    @Type(() => OrganisationCreateDetailsDto)
-    organisationDetails: OrganisationCreateDetailsDto;
+  @ApiProperty()
+  @Type(() => OrganisationCreateDetailsDto)
+  organisationDetails: OrganisationCreateDetailsDto;
 
-    @Type(() => OrgAwardsDetailsDto)
-    orgAwardsDetails: OrgAwardsDetailsDto[];
+  @Type(() => OrgAwardsDetailsDto)
+  orgAwardsDetails: OrgAwardsDetailsDto[];
 
-    @Type(() => OrgTrainingDetailsDto)
-    orgTrainingDetails: OrgTrainingDetailsDto[];
+  @Type(() => OrgTrainingDetailsDto)
+  orgTrainingDetails: OrgTrainingDetailsDto[];
 
-    @Type(() => OrgCourseDetailsDto)
-    orgCourseDetails: OrgCourseDetailsDto[];
+  @Type(() => OrgCourseDetailsDto)
+  orgCourseDetails: OrgCourseDetailsDto[];
 
-    @Type(() => OrgContactDetailsDto)
-    orgContactDetails: OrgContactDetailsDto;
+  @Type(() => OrgContactDetailsDto)
+  orgContactDetails: OrgContactDetailsDto;
 
-    @Type(() => OrgEventDetailsDto)
-    orgEventDetails: OrgEventDetailsDto[];
+  @Type(() => OrgEventDetailsDto)
+  orgEventDetails: OrgEventDetailsDto[];
 }
--- src/organization/dto/organisationOtherDetails.sto.ts
+++ src/organization/dto/organisationOtherDetails.sto.ts
@@ -1,63 +1,66 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 /* eslint-disable prettier/prettier */
 export class OrganisationCreateDetailsDto {
-    [x: string]: any;
-    public fullName: string;
-    public location?: string;
-    public cityId: number;
-    public zipcode?: string;
-    public estaclishedDate?: Date;
-    public affiliation?: string;
-    public logo?: string;
-    public description?: string;
-    public isInstitute: boolean;
-    public isVerified: boolean;
-    public type: string;
+  @ApiProperty()
+  public fullName: string;
+  public location?: string;
+  public cityId: number;
+  public zipcode?: string;
+  public estaclishedDate?: Date;
+  public affiliation?: string;
+  public logo?: string;
+  public description?: string;
+  public isInstitute: boolean;
+  public isVerified: boolean;
+  public type: string;
 }
 export class OrgAwardsDetailsDto {
-    id?: string;
-    title: string;
-    issuedBy: string;
-    statusCode?: number;
-    issuedDate: string;
-    awardsDescription?: string;
-    mediaUrls?: string[];
+  @ApiProperty()
+  id?: string;
+  title: string;
+  issuedBy: string;
+  statusCode?: number;
+  issuedDate: string;
+  awardsDescription?: string;
+  mediaUrls?: string[];
 }
 
 export class OrgTrainingDetailsDto {
-    id?: string;
-    title: string;
-    organizer: string;
-    statusCode?: number;
-    startDate: string;
-    endDate: string;
-    trainingDecs?: string;
-    mediaUrls?: string[];
+  @ApiProperty()
+  id?: string;
+  title: string;
+  organizer: string;
+  statusCode?: number;
+  startDate: string;
+  endDate: string;
+  trainingDecs?: string;
+  mediaUrls?: string[];
 }
 
 export class OrgContactDetailsDto {
-    id?: string;
-    emailId: string;
-    altEmailId: string;
-    contactNumber: string;
-    altContactNum: string;
-    websiteUrl: string;
-
-
+  @ApiProperty()
+  id?: string;
+  emailId: string;
+  altEmailId: string;
+  contactNumber: string;
+  altContactNum: string;
+  websiteUrl: string;
 }
 export class OrgEventDetailsDto {
-    id?: string;
-    title: string;
-    date: string;
-    eventUrl: string;
-    statusCode?: number;
-    desc: string;
+  @ApiProperty()
+  id?: string;
+  title: string;
+  date: string;
+  eventUrl: string;
+  statusCode?: number;
+  desc: string;
 }
 export class OrgCourseDetailsDto {
-    id?: string
-    title: string;
-    affiliatedTo?: string;
-    mediaUrls: string[];
-    statusCode?: number;
-
-
+  @ApiProperty()
+  id?: string;
+  title: string;
+  affiliatedTo?: string;
+  mediaUrls: string[];
+  statusCode?: number;
 }
